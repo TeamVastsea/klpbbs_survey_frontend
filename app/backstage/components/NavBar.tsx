58f3cca0e7aa2f5fc8e3fcbd5c112b14b342b269
JianyueLab <valor@jianyuelab.cc>
2024-08-15 18:14:09 +0900

    BackStage初步UI

---
+++ app/backstage/components/NavBar.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import {
+    IconBellRinging,
+    IconPlus,
+    IconTrash,
+    IconLogout,
+    IconUsersGroup,
+    IconClockHour5,
+
+} from '@tabler/icons-react';
+import classes from '@/app/backstage/components/NavBar.module.css';
+
+const data = [
+    { link: '', label: '信息', icon: IconBellRinging },
+    { link: '', label: '新建问卷', icon: IconPlus },
+    { link: '', label: '最近', icon: IconClockHour5},
+    { link: '', label: '与我合作', icon: IconUsersGroup }
+];
+
+export default function NavBar() {
+    const [active, setActive] = useState('Billing');
+    const links = data.map((item) => (
+        <a
+            className={classes.link}
+            data-active={item.label === active || undefined}
+            href={item.link}
+            key={item.label}
+            onClick={(event) => {
+            event.preventDefault();
+            setActive(item.label);
+            }}
+        >
+            <item.icon className={classes.linkIcon} stroke={1.5} />
+            <span>{item.label}</span>
+        </a>
+    ));
+
+    return (
+        <nav className={classes.navbar}>
+            <div className={classes.navbarMain}>
+                {links}
+            </div>
+
+            <div className={classes.footer}>
+                <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+                    <IconTrash className={classes.linkIcon} stroke={1.5} />
+                    <span>回收站</span>
+                </a>
+
+                <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+                    <IconLogout className={classes.linkIcon} stroke={1.5} />
+                    <span>退出登录</span>
+                </a>
+            </div>
+        </nav>
+    );
+}
