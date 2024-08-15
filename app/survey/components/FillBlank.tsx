3bb0ed6f8f2e2bfdf1e7131bd729ea58bfa0a4d2
Snowball_233 <wangrb.qq@outlook.com>
2024-08-15 16:20:17 +0800

    feat: add data onChange spy

--- app/survey/components/FillBlank.tsx
+++ app/survey/components/FillBlank.tsx
@@ -1,6 +1,14 @@
-import { Textarea } from '@mantine/core';
+import { Text, Textarea } from '@mantine/core';
+import React, { useRef, useState } from 'react';
 
 export function FillBlank() {
+    const [currentValue, setCurrentValue] = useState('');
+    const ref = useRef<HTMLTextAreaElement>(null);
+
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setCurrentValue(event.currentTarget.value);
+    };
+
     return (
         <>
             <Textarea
@@ -9,7 +17,13 @@ export function FillBlank() {
               autosize
               minRows={2}
               maxRows={4}
+              ref={ref}
+              onChange={handleChange}
             />
+
+            <Text fz="xs" mt="md">
+                CurrentValue: {currentValue || '–'}
+            </Text>
         </>
     );
 }
