f946598020298e5e7411473c5abf6c24e0e6c099
Snowball_233 <wangrb.qq@outlook.com>
2024-08-19 01:03:04 +0800

    fix: header, footer, and main page styles

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -14,9 +14,7 @@ import {
     Title,
 } from '@mantine/core';
 import { useRouter } from 'next/navigation';
-import { min } from '@floating-ui/utils';
 import classes from '@/styles/common.module.css';
-import SurveyCard from '@/components/SurveyCard';
 import mainPageImage from '@/public/main-page.svg';
 import github from '@/public/github.svg';
 
@@ -31,137 +29,72 @@ export type Survey = {
 export default function HomePage() {
     const router = useRouter();
 
-    const surveys: Survey[] = [
-        {
-            title: '内群卷卷',
-            version: '2.0船新版本',
-            description: '你好，欢迎参加内群入群问卷，我们希望了解你的需求，以便更好地为你服务。',
-            image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png',
-            link: 'oauth',
-        },
-        {
-            title: '我爱雪球',
-            version: '可爱喵',
-            description: '最喜欢 nya 了',
-            image: 'https://avatars.githubusercontent.com/u/97330394?v=4',
-            link: 'https://github.com/SnowballXueQiu',
-        },
-        {
-            title: 'Test',
-            version: 'Test',
-            description: 'Test',
-            image: 'https://placehold.co/400x400',
-            link: 'https://github.com/SnowballXueQiu',
-        },
-    ];
-
     return (
         <>
-            <div
+            <Center
               style={{
-                    scrollSnapType: 'y mandatory',
-                    overflowY: 'scroll',
-                    height: '100vh',
+                    height: 'calc(100vh - 60px - 56px)',
+                    scrollSnapAlign: 'start',
+                    scrollSnapStop: 'always',
                 }}
             >
-                <Center
-                  style={{
-                        minHeight: '100vh',
-                        scrollSnapAlign: 'start',
-                        scrollSnapStop: 'always',
-                    }}
+                <SimpleGrid
+                  cols={{ base: 1, sm: 2 }}
+                  spacing="lg"
+                  style={{ maxWidth: '80%', width: '100%' }}
                 >
-                    <SimpleGrid
-                      cols={{ base: 1, sm: 2 }}
-                      spacing="lg"
-                      style={{ maxWidth: '80%', width: '100%' }}
-                    >
-                        <Center>
-                            <Stack justify="center">
-                                <Title className={classes.title}>
-                                    <Text span c="#008D57" inherit>
-                                        苦
-                                    </Text>
-                                    <Text span c="#13AE67" inherit>
-                                        力
-                                    </Text>
-                                    <Text span c="#089946" inherit>
-                                        怕
-                                    </Text>
-                                    论坛
-                                </Title>
-                                <Title>&#62; 问卷系统</Title>
-                                <Text fw={500} fz="lg">
-                                    收集更好的数据，作出更好的决策。
+                    <Center>
+                        <Stack justify="center">
+                            <Title className={classes.title}>
+                                <Text span c="#008D57" inherit>
+                                    苦
                                 </Text>
-                                <Space h="md" />
+                                <Text span c="#13AE67" inherit>
+                                    力
+                                </Text>
+                                <Text span c="#089946" inherit>
+                                    怕
+                                </Text>
+                                论坛
+                            </Title>
+                            <Title>&#62; 问卷系统</Title>
+                            <Text fw={500} fz="lg">
+                                收集更好的数据，作出更好的决策。
+                            </Text>
+                            <Space h="md" />
 
-                                <Group>
-                                    <Button
-                                      color="blue"
-                                      component="a"
-                                      href="https://github.com/orgs/TeamVastsea/teams/klpbbs_survey"
-                                      target="_blank"
-                                    >
-                                        <Group gap="xs">
-                                            <Avatar
-                                              src={github.src}
-                                              alt="Github"
-                                              size="1.1rem"
-                                            />
-                                            Github
-                                        </Group>
-                                    </Button>
-                                    <Button color="gray" onClick={() => router.push('/oauth')}>
-                                        立即使用
-                                    </Button>
-                                </Group>
-                            </Stack>
-                        </Center>
-
-                        <Container>
-                            <Image
-                              src={mainPageImage.src}
-                              alt="Main Page Image"
-                              className={classes.image}
-                              style={{ maxWidth: 380, margin: '0 auto' }}
-                            />
-                        </Container>
-                    </SimpleGrid>
-                </Center>
-
-                <Center
-                  style={{
-                        minHeight: '100vh',
-                        scrollSnapAlign: 'start',
-                        scrollSnapStop: 'always',
-                    }}
-                  pb={120}
-                >
-                    <Stack>
-                        <Center>
-                            <Title>现有问卷</Title>
-                        </Center>
-                        <Center>
-                            <SimpleGrid
-                              w="100%"
-                              cols={{
-                                    base: 1,
-                                    md: min(surveys.length, 2),
-                                    lg: min(surveys.length, 4),
-                                }}
-                              spacing={{ base: 'lg', lg: 'xl' }}
-                            >
-                                {surveys.map((survey, index) => (
-                                    <Center key={index}>
-                                        <SurveyCard survey={survey} />
-                                    </Center>
-                                ))}
-                            </SimpleGrid>
-                        </Center>
-                    </Stack>
-                </Center>
-            </div>
+                            <Group>
+                                <Button
+                                  color="blue"
+                                  component="a"
+                                  href="https://github.com/orgs/TeamVastsea/teams/klpbbs_survey"
+                                  target="_blank"
+                                >
+                                    <Group gap="xs">
+                                        <Avatar
+                                          src={github.src}
+                                          alt="Github"
+                                          size="1.1rem"
+                                        />
+                                        Github
+                                    </Group>
+                                </Button>
+                                <Button color="gray" onClick={() => router.push('/oauth')}>
+                                    立即使用
+                                </Button>
+                            </Group>
+                        </Stack>
+                    </Center>
+                    <Container>
+                        <Image
+                          src={mainPageImage.src}
+                          alt="Main Page Image"
+                          className={classes.image}
+                          style={{ maxWidth: 380, margin: '0 auto' }}
+                        />
+                    </Container>
+                </SimpleGrid>
+            </Center>
         </>
     );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,29 +5,27 @@ export default function Footer() {
     const year = new Date().getFullYear();
 
     return (
-        <div>
-            <Container className={classes.inner}>
-                <p>
-                    &copy; {year}&nbsp;
-                    <a
-                      href="https://github.com/TeamVastsea"
-                      target="_blank"
-                      rel="noreferrer"
-                      style={{ textDecoration: 'none' }}
-                    >
-                        瀚海工艺
-                    </a>
-                    &nbsp;|&nbsp;
-                    <a
-                      href="https://klpbbs.com"
-                      target="_blank"
-                      rel="noreferrer"
-                      style={{ textDecoration: 'none' }}
-                    >
-                        苦力怕论坛
-                    </a>
-                </p>
-            </Container>
-        </div>
+        <Container className={classes.inner}>
+            <p>
+                &copy; {year}&nbsp;
+                <a
+                  href="https://github.com/TeamVastsea"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{ textDecoration: 'none' }}
+                >
+                    瀚海工艺
+                </a>
+                &nbsp;|&nbsp;
+                <a
+                  href="https://klpbbs.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{ textDecoration: 'none' }}
+                >
+                    苦力怕论坛
+                </a>
+            </p>
+        </Container>
     );
 }
