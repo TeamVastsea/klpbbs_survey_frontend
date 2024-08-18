24f325a81262e585ea68c55d45c0d07ecba2978c
Snowball_233 <wangrb.qq@outlook.com>
2024-08-18 17:12:46 +0800

    add loading overlay
    Co-Authored-By: zrll_ <46812903+zrll12@users.noreply.github.com>

--- app/(root)/oauth/callback/page.tsx
+++ app/(root)/oauth/callback/page.tsx
@@ -1,25 +1,23 @@
 'use client';
 
-import { Center, Stack, Space } from '@mantine/core';
+import { Center, Stack, Space, LoadingOverlay, Box } from '@mantine/core';
 import { useSearchParams } from 'next/navigation';
 import { notifications } from '@mantine/notifications';
+import { useState } from 'react';
 import UserInfoCard from './components/UserInfoCard';
 import { Cookie } from '@/components/cookie';
 
 export default function CallbackPage() {
     const searchParams = useSearchParams();
 
+    const [username, setUsername] = useState('');
+    const [uid, setUid] = useState('');
+    const [loading, setLoading] = useState(true);
+
     const state = searchParams.get('state');
     const token = searchParams.get('token');
 
     fetch(`https://wj.klpbbs.cn/api/oauth?state=${state}&token=${token}`)
-        .then(() => {
-            notifications.show({
-                title: '账户激活成功',
-                message: undefined,
-                color: 'green',
-            });
-        })
         .then(() => {
             const myHeaders = new Headers();
             myHeaders.append('token', state as string);
@@ -36,6 +34,17 @@ export default function CallbackPage() {
 
                     Cookie.setCookie('uid', user.uid, 7);
                     Cookie.setCookie('username', user.username, 7);
+
+                    setUid(user.uid);
+                    setUsername(user.username);
+                    setLoading(false);
+                })
+                .catch(e => {
+                    notifications.show({
+                        title: '获取用户信息失败，请将以下信息反馈给管理员',
+                        message: e.toString(),
+                        color: 'red',
+                    });
                 });
         })
         .catch(e => {
@@ -46,14 +55,14 @@ export default function CallbackPage() {
             });
         });
 
-    const uid = Cookie.getCookie('uid');
-    const username = Cookie.getCookie('username');
-
     return (
         <Center>
             <Stack>
                 <Space h={100} />
-                <UserInfoCard id={uid} username={username} />
+                <Box pos="relative">
+                    <UserInfoCard id={uid} username={username} />
+                    <LoadingOverlay visible={loading} overlayProps={{ radius: 'sm', blur: 2 }} />
+                </Box>
                 <Space h={100} />
             </Stack>
         </Center>
