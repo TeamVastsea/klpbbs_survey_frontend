9e7ecfd2504b7cffb879bfc926ef9430e3d968bf
M397749490 <M397749490@outlook.com>
2024-08-22 02:34:46 +0800

    chore: add link to text

--- app/(root)/oauth/components/LoginBanner.tsx
+++ app/(root)/oauth/components/LoginBanner.tsx
@@ -1,12 +1,14 @@
 import { Text, Title, Button, Image, Space, Container, SimpleGrid } from '@mantine/core';
 import { useState } from 'react';
 import { notifications } from '@mantine/notifications';
+import { useRouter } from 'next/navigation';
 import image from './logo.png';
 import classes from './LoginBanner.module.css';
 import { Cookie } from '@/components/cookie';
 
 export function LoginBanner() {
     const [loading, setLoading] = useState(false);
+    const router = useRouter();
 
     function getLogin() {
         setLoading(true);
@@ -44,12 +46,12 @@ export function LoginBanner() {
                             苦力怕论坛
                         </Title>
                         <Text fw={500} fz="lg" mb={5}>
-                            请授权我们登录您的苦力怕论坛账号
+                            请授权我们获取您的苦力怕论坛信息
                         </Text>
                         <Text fz="sm" c="dimmed">
-                            您的账号仅用于确认您的身份，我们不会将您的账号用于任何其他目的或与任何第三方共享。详细请参见我们的
-                            <Text c="blue" td="underline" style={{ cursor: 'pointer' }} component="span" inherit>
-                                隐私政策
+                            您的信息仅用于确认您的身份，我们不会将您的信息用于任何其他目的或与任何第三方共享。详细请参见我们的
+                            <Text c="blue" td="underline" onClick={() => router.push('/tos')} style={{ cursor: 'pointer' }} component="span" inherit>
+                                服务条款
                             </Text>
                         </Text>
                         <Space h={30} />
