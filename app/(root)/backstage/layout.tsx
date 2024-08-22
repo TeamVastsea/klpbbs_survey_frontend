e3c093357cc409808525962d1e35b4cd4ea2abfa
Snowball_233 <wangrb.qq@outlook.com>
2024-08-23 00:46:01 +0800

    fix: unuse debug code and format

--- app/(root)/backstage/layout.tsx
+++ app/(root)/backstage/layout.tsx
@@ -5,15 +5,13 @@ import Watermark from '@/app/(root)/backstage/components/Watermark';
 import { Cookie } from '@/components/cookie';
 
 export default function BackStageLayout({ children }: { children: React.ReactNode }) {
-    const userName = Cookie.getCookie('username');
-    const userId = Cookie.getCookie('uid');
-
-    console.log(document.cookie);
+    const userName = decodeURI(Cookie.getCookie('username') || '');
+    const userId = Cookie.getCookie('uid') || '';
 
     return (
         <div>
             <Watermark
-              text={`${decodeURI(userName)} ${userId}`}
+              text={`${userName} ${userId}`}
               fontSize={20}
               gap={5}
             >
