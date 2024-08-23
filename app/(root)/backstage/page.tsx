1592fce80f330cbf5ebc0a24fd8da3ae66d54cb1
Snowball_233 <wangrb.qq@outlook.com>
2024-08-23 22:33:08 +0800

    feat: add backstage main page

--- app/(root)/backstage/page.tsx
+++ app/(root)/backstage/page.tsx
@@ -1,12 +1,66 @@
 'use client';
 
+import { Center, Divider, Stack, Title, Button, Text } from '@mantine/core';
+import { IconFilePlus, IconEdit, IconChecklist } from '@tabler/icons-react';
+import { useEffect, useState } from 'react';
+import { Cookie } from '@/components/cookie';
+
 export default function Backstage() {
+    const [userName, setUserName] = useState('');
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const userNameFromCookie = Cookie.getCookie('username') || 'unknown';
+            setUserName(decodeURI(userNameFromCookie));
+        }
+    }, []);
+
+    // 跳转到指定路径
+    const handleNavigation = (path: string) => {
+        window.location.href = path;
+    };
+
     return (
-        <div>
-            <h1>Backstage</h1>
-            <a href="/backstage/editor">editor</a>
-            <br />
-            <a href="/backstage/judge">judge</a>
-        </div>
+        <>
+            <Center>
+                <Stack w="80%">
+                    <Title>
+                        问卷系统
+                    </Title>
+                    <Title>
+                        后台管理界面
+                    </Title>
+                    <Divider />
+                    <Text>
+                        尊敬的管理员 {userName}, 您好!
+                    </Text>
+                    <Center h={400}>
+                        <Stack w="60%">
+                            <Button
+                              size="xl"
+                              leftSection={<IconFilePlus />}
+                              onClick={() => handleNavigation('/editor/new')}
+                            >
+                                新建问卷
+                            </Button>
+                            <Button
+                              size="xl"
+                              leftSection={<IconEdit />}
+                              onClick={() => handleNavigation('/backstage/editor')}
+                            >
+                                编辑问卷
+                            </Button>
+                            <Button
+                              size="xl"
+                              leftSection={<IconChecklist />}
+                              onClick={() => handleNavigation('/editor/judge')}
+                            >
+                                批改问卷
+                            </Button>
+                        </Stack>
+                    </Center>
+                </Stack>
+            </Center>
+        </>
     );
 }
