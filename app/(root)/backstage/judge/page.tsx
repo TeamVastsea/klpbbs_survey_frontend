366c838dab0e957ea0b6d933224cf06094979b20
Snowball_233 <wangrb.qq@outlook.com>
2024-08-25 02:02:14 +0800

    feat: Judge main page primary

--- app/(root)/backstage/judge/page.tsx
+++ app/(root)/backstage/judge/page.tsx
@@ -1,14 +1,39 @@
 'use client';
 
-export default function JudgePage({ params }: { params: { id: number } }) {
-    const { id } = params;
-    console.log(id);
+import { Input, Stack, Title, Center, Button } from '@mantine/core';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function SurveyPage() {
+    const [answerId, setAnswerId] = useState<number | undefined>(undefined);
+    const router = useRouter();
 
     return (
-        <div>
-            <h1>Judge</h1>
-            <h2>{id}</h2>
-        </div>
+        <Center>
+            <Stack w="90%">
+                <Center>
+                    <Title>
+                        阅卷系统
+                    </Title>
+                </Center>
+                <Center h="100%">
+                    <Stack>
+                        <Input.Wrapper label="请输入 Answer ID 以跳转">
+                            <Input
+                              onChange={(e) => setAnswerId(parseInt(e.currentTarget.value, 10))}
+                            />
+                        </Input.Wrapper>
+                        <Center>
+                            <Button
+                              onClick={() => { router.push(`/backstage/judge/${answerId}`); }}
+                            >
+                                确认跳转至 {answerId}
+                            </Button>
+                        </Center>
+                    </Stack>
+                </Center>
+            </Stack>
+        </Center>
 
     );
 }
