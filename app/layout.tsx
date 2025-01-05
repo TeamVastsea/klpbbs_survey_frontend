6773eb854b879bafc29a7af5a92fef7e36d9aec3
zrll_ <46812903+zrll12@users.noreply.github.com>
2025-01-05 14:20:05 +0800

    init

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import '@mantine/core/styles.css';
+
+import React from 'react';
+import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core';
+import { theme } from '../theme';
+
+export const metadata = {
+  title: 'Mantine Next.js template',
+  description: 'I am using Mantine with Next.js!',
+};
+
+export default function RootLayout({ children }: { children: any }) {
+  return (
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+      </head>
+      <body>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
